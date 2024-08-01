a0ba3cfe99bd8ae84a400c16f7101697c52d8685
RRoot <rootr@uw.edu>
2024-07-31 18:30:32 -0700

    Add files via upload

---
+++ Category Sales for 1997.sql
@@ -0,0 +1,11 @@
+
+USE [Northwind]
+GO
+
+CREATE VIEW [dbo].[Category Sales for 1997] AS
+SELECT "Product Sales for 1997".CategoryName, Sum("Product Sales for 1997".ProductSales) AS CategorySales
+FROM "Product Sales for 1997"
+GROUP BY "Product Sales for 1997".CategoryName
+GO
+
+
